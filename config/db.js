51457ce00cdf2d5980f3240d7f3f9debac4f253f
Codar-Boy <103827533+Codar-Boy@users.noreply.github.com>
2026-05-11 00:25:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,27 @@
+//const { Pool } = require('pg');
+//require('dotenv').config();
+
+//const pool = new Pool({
+//  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+//  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+//});
+
+//module.exports = pool;
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const connectionString =
+  process.env.DATABASE_URL ||
+  process.env.DB_URL;
+
+console.log("DATABASE URL EXISTS:", !!connectionString);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
